41f03039df65216e777a587cb60b657ba0ad8e2b
smidm <smidmatej99@gmail.com>
2022-12-29 15:56:57 +0100

    Created the Explorer class.

---
+++ src/Explorer.py
@@ -0,0 +1,12 @@
+
+
+class Explorer:
+    def __init__(self) -> None:
+        velocities_to_explore = [0,5,10,15,20]*3 
+        
+
+    def get_explored_velicities_from_gpe(self, gpe):
+        gpe
+        D_to_explore = (max(De[vx]) + 5, max(De[vy]) + 5, max(De[vz]) + 5) # Here I will probably need to use inducing points
+        
+
