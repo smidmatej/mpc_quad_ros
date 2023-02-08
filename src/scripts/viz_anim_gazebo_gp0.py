6b3e926eb31d3dd1239468b47be698480154b37a
smidm <smidmatej99@gmail.com>
2023-02-08 13:23:50 +0100

    Fixed scripts dependencies

--- src/scripts/viz_anim_gazebo_gp0.py
+++ src/scripts/viz_anim_gazebo_gp0.py
@@ -1,4 +1,7 @@
 import os
+import sys
+sys.path.append('../')
+
 from Visualiser import Visualiser
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
--- src/scripts/viz_plot_gazebo_gp2.py
+++ src/scripts/viz_plot_gazebo_gp2.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.append('../')
 from Visualiser import Visualiser
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
