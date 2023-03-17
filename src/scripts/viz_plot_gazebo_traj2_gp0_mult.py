f86da47d7878eb10e7d65640c2dcce464acae57c
smidm <smidmatej99@gmail.com>
2023-03-17 16:23:00 +0100

    Generated static trajectories

--- src/scripts/viz_plot_gazebo_traj2_gp0_mult.py
+++ src/scripts/viz_plot_gazebo_traj2_gp0_mult.py
@@ -7,7 +7,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 dir_path = os.path.join(dir_path, '..') # go up one level
 environment = 'gazebo_simulation'
 
-filenames = ['traj2_v3_a3_gp0','traj2_v6_a6_gp0','traj2_v9_a9_gp0','traj2_v12_a12_gp0']
+filenames = ['traj0_v3_a3_gp2','traj0_v6_a6_gp2','traj0_v9_a9_gp2','traj0_v12_a12_gp2']
 for data_filename in filenames:
     trajectory_filename = os.path.join(dir_path, '..', 'outputs', environment, 'data', data_filename + '.pkl')
     result_plot_filename = os.path.join(dir_path, '..', 'outputs', 'graphics', 'plot_' + environment + '_' + data_filename + '.pdf')
